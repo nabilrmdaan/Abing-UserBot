90567d231cdd79bc6297302da34479d4cea8997b
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-12 01:42:35 +0700

    Update decorator.py

--- userbot/utils/decorator.py
+++ userbot/utils/decorator.py
@@ -55,7 +55,7 @@ def bing_cmd(
         else:
             bing_ = "\\" + CMD_HANDLER
             sudo_ = "\\" + SUDO_HANDLER
-            bing_reg = re.compile(kyy_ + pattern)
+            bing_reg = re.compile(bing_ + pattern)
             sudo_reg = re.compile(sudo_ + pattern)
             if command is not None:
                 cmd1 = bing_ + command
