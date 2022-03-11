23cb41e379c32772496dbb2447a3ab4887c2b8b9
Abing <97945597+nabilrmdaan@users.noreply.github.com>
2022-03-11 21:00:01 +0700

    Update fakeAction.py

--- userbot/modules/fakeAction.py
+++ userbot/modules/fakeAction.py
@@ -1,12 +1,12 @@
-# Port By @IDnyaKosong From Kyy-Userbot
-# # Copyright (C) 2021 Kyy-Userbot
+# Port By @sayaabing From Abing-Userbot
+# # Copyright (C) 2022 Abing-Userbot
 
-from userbot.utils import edit_or_reply, edit_delete, kyy_cmd
+from userbot.utils import edit_or_reply, edit_delete, bing_cmd
 from userbot import CMD_HELP, CMD_HANDLER as cmd
 import asyncio
 
 
-@kyy_cmd(pattern="ftyping(?: |$)(.*)")
+@bing_cmd(pattern="ftyping(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -24,7 +24,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="faudio(?: |$)(.*)")
+@bing_cmd(pattern="faudio(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -42,7 +42,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="fvideo(?: |$)(.*)")
+@bing_cmd(pattern="fvideo(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -60,7 +60,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="fgame(?: |$)(.*)")
+@bing_cmd(pattern="fgame(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -78,7 +78,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="fround(?: |$)(.*)")
+@bing_cmd(pattern="fround(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -98,7 +98,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="fphoto(?: |$)(.*)")
+@bing_cmd(pattern="fphoto(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -118,7 +118,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="fdocument(?: |$)(.*)")
+@bing_cmd(pattern="fdocument(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -139,7 +139,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="flocation(?: |$)(.*)")
+@bing_cmd(pattern="flocation(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
@@ -159,7 +159,7 @@ async def _(event):
         await asyncio.sleep(t)
 
 
-@kyy_cmd(pattern="fcontact(?: |$)(.*)")
+@bing_cmd(pattern="fcontact(?: |$)(.*)")
 async def _(event):
     t = event.pattern_match.group(1)
     if not (t or t.isdigit()):
